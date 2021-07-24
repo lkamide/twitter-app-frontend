3d05f598cbb9866e249b965a6517e1542a0532a7
lkamide <lucaskamide@outlook.com>
2021-07-23 21:24:09 -0300

    changed the home to use useState, UX improved

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,127 +1,118 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import api from '../services/apis';
-import { Button, FormGroup, TextInput, FormLabel, Grid, Column, Row } from 'carbon-components-react';
+import { FormLabel, Grid, Column, Row, InlineLoading, Search } from 'carbon-components-react';
 import Chart from './chart';
-import logo from '../twitter.svg';
+import { Location16 , Chat16} from '@carbon/icons-react';
 
-function Home() {
-  return class extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: '',
-        _id: '',
-        username: '',
-        twitte: [{}],
-        twitteCount:[],
-        lastTweets:[]
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
+export default function Home() {
+  const [value, setValue] = useState('');
+  const [twitte, setTwitte] = useState([{}]);
+  const [twitteCount, setTwitteCount] = useState([]);
+  const [lastTweets, setLastTweets] = useState([]);
 
-    handleChange(event) {
-      this.setState({ value: event.target.value });
-    }
-    handleSubmit(event) {
-      this.setState({ 
-        twitte: [{}],
-        twitteCount:[],
-        lastTweets:[]
-      });
-      
-      api.get(`/userInformation/${this.state.value}`).then(response => {
-        console.log(response)
+  const [isSearching, setIsSearching] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [description, setDescription] = useState('Searching...');
+  const [ariaLive, setAriaLive] = useState('off');
 
-          response.data.data.statuses.forEach(data => {
-
-            this.setState({
-              twitte: [...this.state.twitte, {text : data.text, location: data.user.location }]
-            });
-          })
-          response.data.count.data.forEach(data => {
-
-            this.setState({
-              twitteCount: [...this.state.twitteCount, {date : data.start, value: data.tweet_count, group: "user" }]
-            });
-          })
-          response.data.lastTweets.data.forEach(data => {
-
-            this.setState({
-              lastTweets: [...this.state.lastTweets, {text : data.text}]
-            });
-          })
-
-        
-      })
-      event.preventDefault();
-    }
-
-
-    render() {
-      return <>
-        <Grid narrow>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            {/* <img className="App-logo" alt="twiiter-Logo" src={TwitterLogo} /> */}
-          </header>
-          </div>
-          <Row sm={1}>
-            <TextInput sm={1} id="input" labelText='Type here the user ' value={this.state.value} onChange={this.handleChange} />
-            <Button onClick={this.handleSubmit} type="submit">Submit</Button>
-          </Row>
-
-          <Row >
-
-            <Column>
-
-              <FormGroup legendText="" >
-                Last Mentionings/#: 
-                <br/>
-                { this.state.twitte.length >0 && this.state.twitte.map((twitte, index) => {
-                  // console.log("size", this.state.twitte.length)
-                  return <>
-                  {twitte.text && <FormLabel key={twitte.text + index} > text : {twitte.text} </FormLabel> }
-                  {twitte.location && <FormLabel key={twitte.location + index} > location : {twitte.location} </FormLabel> }
-                  </>
-                })}
-              </FormGroup>
-
-            </Column>
-
-            <Column>
-
-              Last twittes 
-              <br/>
-              {this.state.lastTweets.map(twitte => {
-                  // console.log("size", this.state.lastTweets.length)
-                  return <>
-                  {twitte.text && <FormLabel key={twitte.text}> text : {twitte.text} </FormLabel> }
-                  </>
-                })}
-
-            </Column>
-
-            <Column>
-              
-              tweet count query= {this.state.value}
-
-              <Chart key={this.state.twitteCount} counts={this.state.twitteCount} />
-
-            </Column>
-
-            {/* <Column>
-
-              1/4
-
-            </Column> */}
-
-          </Row>
-
-        </Grid>
-      </>
+  const handleEnter = (event) => {
+    if (event.key === 'Enter') {
+      handleSubmit(event)
     }
   }
+
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+  const handleSubmit = async (event) => {
+    setTwitte([{}])
+    setTwitteCount([])
+    setLastTweets([])
+
+    setIsSearching(true);
+
+    await api.get(`/userInformation/${value}`).then(response => {
+      console.log(response)
+      setSuccess(true);
+      setAriaLive('assertive');
+      setDescription('mounting..');
+
+      response.data.data.statuses.forEach(data => {
+        setTwitte(twitte => [...twitte, { text: data.text, location: data.user.location }])
+      })
+      response.data.count.data.forEach(data => {
+        setTwitteCount(twitteCount => [...twitteCount, { date: data.start, value: data.tweet_count, group: "user" }])
+      })
+      response.data.lastTweets.data.forEach(data => {
+        setLastTweets(lastTweets => [...lastTweets, { text: data.text }])
+      })
+
+    }).then(() => {
+      setIsSearching(false);
+      setSuccess(false);
+      setAriaLive('off');
+    })
+    event.preventDefault();
+  }
+
+
+  return (
+    <>
+      <Grid >
+
+        <Row className="coluna">
+          <Search placeHolderText="type here the user..." onChange={handleChange} onKeyPress={handleEnter} />
+          {isSearching &&
+            <InlineLoading
+              style={{ marginLeft: '1rem' }}
+              description={description}
+              status={success ? 'finished' : 'active'}
+              aria-live={ariaLive}
+            />
+          }
+        </Row>
+        <Row >
+          <Column className="coluna">
+            <br />
+            <h4>
+              Last mentionings:
+            </h4>
+            <br />
+            {twitte.length > 0 && twitte.map((twitte, index) => {
+              return <>
+              <div >
+                <br/>
+                {twitte.text && <FormLabel key={twitte.text + index} >  {twitte.text}  </FormLabel>}
+                <br/>
+                {twitte.location && <FormLabel className="bold" key={twitte.location + index} > <Location16 fill="black" /> {twitte.location} </FormLabel>}
+              </div>
+              </>
+            })}
+
+          </Column>
+          <Column className="coluna">
+            <br />
+            <h4>Last twittes: </h4>
+            <br />
+            {lastTweets.map(twitte => {
+              return <>
+                <br/>
+                <br/>
+                {twitte.text && <FormLabel key={twitte.text}>{twitte.text}</FormLabel>}
+              </>
+            })}
+          </Column>
+          <Column className="coluna">
+            <br />
+            <h4>
+              Tweet count query: {value}
+            </h4>
+            <br />
+            <Chart key={twitteCount} counts={twitteCount} />
+          </Column>
+        </Row>
+      </Grid>
+    </>
+  )
 }
-export default Home()
