0536aec106bc3dc0331fa3d268bc0db0eb62a0b5
lkamide <lucaskamide@outlook.com>
2021-07-23 21:23:39 -0300

    removed or commented unused code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import logo from './twitter.svg';
-// import './App.scss';
-
 import Home from './components/home'
 import HeaderComponent from './components/header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <HeaderComponent />
-        <Home/>
-      </header>
+    <div >
+      <HeaderComponent />
+      <Home/>
     </div>
   );
 }
