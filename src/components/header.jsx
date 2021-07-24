db40c36e1a4204094dcbfbced263782dde14e52b
lkamide <lucaskamide@outlook.com>
2021-07-23 21:22:39 -0300

    header improved

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
-import { HeaderName, Header } from 'carbon-components-react';
+import { HeaderName, Header, Row, Grid } from 'carbon-components-react';
+import logo from '../twitter.svg';
+
+import './style.css'
 
 function HeaderComponent() {
   return class extends Component {
 
- 
+
     render() {
-      return <Header aria-label="IBM Platform Name">
-
-      <HeaderName href="#" prefix="">
-
-       Twitter
-
-      </HeaderName>
-
-      </Header>
+      return <>
+        <Header aria-label="IBM Platform Name">
+          <HeaderName href="#" prefix="">
+            Twitter
+          </HeaderName>
+        </Header>
+        <div className="App">
+          <header className="headerColor">
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <Row>
+              <div className="bx--grid bx--grid--no-gutter bx--grid--full-width">
+                <div className="">
+                  <img src={logo} className="App-logo" alt="logo" />
+                </div>
+                <h3>Welcome to the Twitter application</h3>
+              </div>
+            </Row>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+          </header>
+        </div>
+        <br />
+      </>
     }
   }
 }
