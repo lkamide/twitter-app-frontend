ac9176ece0adf4072ccc67bcc855caa9eac07acb
lkamide <lucaskamide@outlook.com>
2021-07-28 17:41:33 -0300

    added notification, improving UX, showing user profile image

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,17 +3,23 @@ import api from '../services/apis';
 import { FormLabel, Grid, Column, Row, InlineLoading, Search } from 'carbon-components-react';
 import Chart from './chart';
 import { Location16 } from '@carbon/icons-react';
+import { User } from '@carbon/pictograms-react';
+import Notification from './notification';
+
 
 export default function Home() {
   const [value, setValue] = useState('');
   const [twitte, setTwitte] = useState([{}]);
   const [twitteCount, setTwitteCount] = useState([]);
   const [lastTweets, setLastTweets] = useState([]);
+  const [user, setUser] = useState({});
 
   const [isSearching, setIsSearching] = useState(false);
   const [success, setSuccess] = useState(false);
   const [description, setDescription] = useState('Searching...');
   const [ariaLive, setAriaLive] = useState('off');
+  const [notification, setNotification] = useState({});
+
 
   const handleEnter = (event) => {
     if (event.key === 'Enter') {
@@ -21,14 +27,33 @@ export default function Home() {
     }
   }
 
+  const setToast = ()=>{
+    setNotification(
+      {
+        enable:true,
+        kind: "error",
+        title: "Not able to find the User",
+        subtitle: "Please check if you type the correct user"
+      }
+      )
+    setTimeout(function(){ setNotification({}) }, 5000);
+  }
+
 
   const handleChange = (event) => {
+    if (event.target.ariaLabel === "Clear search input" ){
+      setTwitte([{}])
+      setTwitteCount([])
+      setLastTweets([])
+      setUser({})
+    }
     setValue(event.target.value)
   }
   const handleSubmit = async (event) => {
     setTwitte([{}])
     setTwitteCount([])
     setLastTweets([])
+    setUser({})
 
     setIsSearching(true);
 
@@ -37,6 +62,8 @@ export default function Home() {
       setSuccess(true);
       setAriaLive('assertive');
       setDescription('mounting..');
+
+      setUser(response.data.user)
 
       response.data.data.statuses.forEach(data => {
         setTwitte(twitte => [...twitte, { text: data.text, location: data.user.location }])
@@ -52,6 +79,11 @@ export default function Home() {
       setIsSearching(false);
       setSuccess(false);
       setAriaLive('off');
+    }).catch((error) =>{
+      setToast()
+      setIsSearching(false);
+      setSuccess(false);
+      setAriaLive('off');
     })
     event.preventDefault();
   }
@@ -60,7 +92,7 @@ export default function Home() {
   return (
     <>
       <Grid >
-
+      {Object.keys(notification).length > 0 && <Notification notification={notification} />  }
         <Row className="coluna">
           <Search placeHolderText="type here the user..." onChange={handleChange} onKeyPress={handleEnter} />
           {isSearching &&
@@ -73,20 +105,36 @@ export default function Home() {
           }
         </Row>
         <Row >
+          <Column>
+            {!user.profile_image ?
+              <User className="App-logo user-logo" /> :
+              <div className="user-info" >
+                <img src={user.profile_image} className="App-logo user-logo" alt="logo" />
+                <hr />
+                <h5>{user.name}</h5>
+                <br /> {user.description} <br />
+                <br /> followers: {user.followers} <br />
+                <br /> following: {user.following} <br /><br />
+                <Location16 />{user.location} <br />
+                <br />
+                {user.created_at ?  "created: " +  new Date(user.created_at).toLocaleDateString() : ""}
+                <hr />
+              </div>
+            }
+          </Column>
           <Column className="coluna">
             <br />
             <h4>
               Last mentionings:
             </h4>
-            <br />
             {twitte.length > 0 && twitte.map((twitte, index) => {
               return <>
-              <div >
-                <br/>
-                {twitte.text && <FormLabel key={twitte.text + index} >  {twitte.text}  </FormLabel>}
-                <br/>
-                {twitte.location && <FormLabel className="bold" key={twitte.location + index} > <Location16 fill="black" /> {twitte.location} </FormLabel>}
-              </div>
+                <div >
+                  <br />
+                  {twitte.text && <FormLabel key={twitte.text + index} >  {twitte.text}  </FormLabel>}
+                  <br />
+                  {twitte.location && <FormLabel className="bold" key={twitte.location + index} > <Location16 fill="black" /> {twitte.location} </FormLabel>}
+                </div>
               </>
             })}
 
@@ -97,8 +145,8 @@ export default function Home() {
             <br />
             {lastTweets.map(twitte => {
               return <>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 {twitte.text && <FormLabel key={twitte.text}>{twitte.text}</FormLabel>}
               </>
             })}
