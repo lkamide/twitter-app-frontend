10af0154ab1f990a392aa5566b7782482ef60a77
lkamide <lucaskamide@outlook.com>
2022-01-14 17:28:00 -0300

    added video, and redirect link

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import api from '../services/apis';
-import { FormLabel, Grid, Column, Row, InlineLoading, Search, Modal } from 'carbon-components-react';
+import { FormLabel, Grid, Column, Row, InlineLoading, Search } from 'carbon-components-react';
 import Chart from './chart';
 import { Location16 } from '@carbon/icons-react';
 import { User } from '@carbon/pictograms-react';
@@ -20,7 +20,6 @@ export default function Home() {
   const [ariaLive, setAriaLive] = useState('off');
   const [notification, setNotification] = useState({});
   const [image, setImage] = useState("");
-
 
   const handleEnter = (event) => {
     if (event.key === 'Enter') {
@@ -66,19 +65,25 @@ export default function Home() {
       setUser(response.data.user)
 
       response.data.data.statuses.forEach(data => {
-        setTwitte(twitte => [...twitte, { text: data.text, location: data.user.location, media: data.extended_entities?.media[0].media_url }])
+        let attach = data.extended_entities?.media[0].video_info ? { video: data.extended_entities?.media[0].video_info?.variants[0].url} : {media: data.extended_entities?.media[0]?.media_url}
+        setTwitte(twitte => [...twitte, { text: data.text, location: data.user.location, ...attach  } ])
       })
       response.data.count.data.forEach(data => {
         setTwitteCount(twitteCount => [...twitteCount, { date: data.start, value: data.tweet_count, group: "user" }])
       })
       response.data.lastTweets.data.forEach(data => {
         let attach_url 
+        let url_shared
         if(data.attachments){
           response.data.lastTweets.includes.media.find( (e) => { 
               return e.media_key ===  data.attachments.media_keys[0] ? attach_url = e.url : ""
           } ) 
         }
-        setLastTweets(lastTweets => [...lastTweets, { text: data.text , attach: attach_url }])
+      if ( data.entities && data.entities.urls &&typeof(data.entities.urls)!="string" ){
+        url_shared = { url: data.entities?.urls?.[0]?.unwound_url || "" , img: data.entities?.urls?.[0]?.images?.[0].url || ""  }
+      }
+
+        setLastTweets(lastTweets => [...lastTweets, { text: data.text , attach: attach_url, url: url_shared }])
       })
 
     }).then(() => {
@@ -94,11 +99,10 @@ export default function Home() {
     event.preventDefault();
   }
 
-
   return (
     <>
       <Grid  >
-        {image && <Lightbox onClose={e=>setImage("")} image={image}/>  }
+      {image && <Lightbox onClose={e=>setImage("")} image={image}/>  }
       {Object.keys(notification).length > 0 && <Notification notification={notification} />  }
         <Row className="coluna">
           <Search placeHolderText="type here the user..." onChange={handleChange} onKeyPress={handleEnter} />
@@ -145,8 +149,11 @@ export default function Home() {
               return <>
                 <div >
                   <br />
-                  {twitte.text && <FormLabel key={twitte.text + index} >  {twitte.text} {twitte.media}  </FormLabel>}
+                  {twitte.text && <FormLabel style={{ width: "100%" }} key={twitte.text + index} >  {twitte.text}</FormLabel>}
                   {twitte.media  && <img className="tweet-image" src={twitte.media} alt={"logo"}  onClick={() => setImage(twitte.media) } />  }
+                  {twitte.video  &&  
+                    <video width="100%" height="240" controls> <source src={twitte.video} type="video/mp4" /> </video> 
+                    }
                   <br />
                   {twitte.location && <FormLabel className="bold" key={twitte.location + index} > <Location16 fill="black" /> {twitte.location} </FormLabel>}
                 </div>
@@ -157,16 +164,27 @@ export default function Home() {
           <Column className="coluna" sm={4} md={2} lg={4}>
             <br />
             <h4>Last twittes: </h4>
+           
             <br />
             {lastTweets.map(twitte => {
               return <>
                 <br />
                 <br />
-                {twitte.text && <FormLabel key={twitte.text}>{twitte.text} {twitte.attach} </FormLabel>}
-                {twitte.attach && 
-                  <img className="tweet-image" src={twitte.attach} alt={"logo"}  onClick={() => setImage(twitte.attach) } /> 
-                }
-        
+                {twitte.text && <FormLabel style={{width:"100%"}} key={twitte.text}>{twitte.text}</FormLabel>}
+                {twitte.attach && <img className="tweet-image" src={twitte.attach} alt={"logo1"}  onClick={() => setImage(twitte.attach) } />  }
+                { twitte.url && twitte.url.img && <div 
+                onClick={() => window.location.href=twitte.url.url }
+                style={{ 
+                  cursor:"pointer",
+                  height: "212px",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                  width:"100%",
+                  backgroundImage: `url(${twitte.url.img})` 
+                }}>
+                  </div>
+                 
+                 }
               </>
             })}
           </Column>
